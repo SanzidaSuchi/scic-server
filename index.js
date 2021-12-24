ee1efcc735a493c9b32a6cd9cfd70f21f09a6132
Sanzida Suchi <sanzidasuchi356@gmail.com>
2021-12-24 18:54:19 +0600

    finished

--- index.js
+++ index.js
@@ -39,6 +39,90 @@ client.connect(err => {
         const services = await cursor.toArray();
         res.send(services);
     });
+    
+        //post order data api //
+        app.post('/orders', async(req, res) =>{
+          const order = req.body;
+
+          const result = await ordersCollection.insertOne(order);
+          console.log(`A document was inserted with the _id: ${result.insertedId}`);
+          res.json(result)
+      });
+    
+           // get orders api //
+      app.get('/orders', async (req, res) => {
+        const cursor = ordersCollection.find({});
+        const orders = await cursor.toArray();
+        res.send(orders);
+    });
+    // confirmation put api 
+    app.put("/confirmation/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const order = {
+        $set: {
+          status:"Confirm"
+        },
+      };
+        const result = await ordersCollection.updateOne(query,order);
+        res.json(result);
+        console.log(result);
+    });
+    // app.get("/myOrder/:email", async (req, res) => {
+    //   console.log(req.params.email);
+    //   const result = await ordersCollection
+    //     .find({ email: req.params.email })
+    //     .toArray();
+    //   res.send(result);
+    // });
+     // review
+  app.post("/addSReview", async (req, res) => {
+    const result = await reviewCollection.insertOne(req.body);
+    res.send(result);
+  });
+     // add services
+  app.post("/services", async (req, res) => {
+    console.log(req.body);
+    const result = await servicesCollection.insertOne(req.body);
+    console.log(result);
+  });
+
+   // delete data from cart delete api
+   app.delete("/delete/:id", async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await ordersCollection.deleteOne(query);
+    res.json(result);
+  });
+
+  app.post("/addUserInfo", async (req, res) => {
+    console.log("req.body");
+    const result = await usersCollection.insertOne(req.body);
+    res.send(result);
+    console.log(result);
+  });
+  //  make admin
+
+  app.put("/makeAdmin", async (req, res) => {
+    const email =  req.body.email;
+    const filter = { email };
+      const documents = await usersCollection.updateOne(filter, {
+        $set: { role: "admin" },
+      },{upsert:true});
+      console.log(documents);
+  
+    
+  });
+
+  // check admin or not
+  app.get("/checkAdmin/:email", async (req, res) => {
+    const result = await usersCollection
+      .find({ email: req.params.email })
+      .toArray();
+    console.log(result);
+    res.send(result);
+  });
+
 
 
 });
